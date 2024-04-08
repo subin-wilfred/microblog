8eae374328eec0a6b719c0bad229e4fc318b240f
SUBIN WILFRED <subin.wilfred@lntecc.com>
2024-04-08 17:35:33 +0530

    Added user profile page and avatars

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ import sqlalchemy.orm as so
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -22,6 +23,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=retro&s={size}'
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)

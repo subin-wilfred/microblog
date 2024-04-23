e7e04690cd47893956880eaeeb668b2a9181de99
SUBIN WILFRED <subin.wilfred@lntecc.com>
2024-04-23 16:36:20 +0530

    Added submit blog posts and pagination

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['chimichaanga7@gmail.com']
+    POSTS_PER_PAGE = 25

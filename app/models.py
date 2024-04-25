761b716baeee1345c07ec0ac009ea687479ac538
SUBIN WILFRED <subin.wilfred@lntecc.com>
2024-04-25 18:03:48 +0530

    Updated password reset, added asynchronous email

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timezone
+from time import time
+import jwt
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
@@ -86,6 +88,22 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode({
+            'reset_password': self.id, 
+            'exp': time() + expires_in
+        },
+        app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], 
+                algorithms=['HS256'])['reset_password']
+        except: 
+            return
+        return db.session.get(User, id)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
